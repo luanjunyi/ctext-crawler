7ce6ba8ccbd19f355da84b9e871cb17e0c32a400
Jerry Luan <xmaswillyou@gmail.com>
2023-11-23 17:57:13 -0800

    core function is ready

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # ctext-crawler
-Crawl the book content from ctext.org into JSON format
+ 
+Crawl the book content from ctext.org into JSON format.
+
+For example,
+
+```bash
+python crawl.py --url="https://ctext.org/huangdi-neijing/ling-shu-jing/zhs" 
+    --title="黄帝内经 - 灵枢经" 
+    --chapter-filter-regex="huangdi-neijing/.+/zhs"
+```
+
+This will crawl the book 灵枢经 from its root URL(ctext.org/huangdi-neijing/ling-shu-jing/zhs) and store the result in "黄帝内经 - 灵枢经.json".
+
+
+## Provide URL filter regex
+
+You need to provide the regular expression for each chapter URL in `--chapter-filter-regex`. This regex is used to distinguish chapter links from the root page.
+
+For example, the chapter links from the root page of 灵枢经 are all like `https://ctext.org/huangdi-neijing/xie-qi-cang-fu-bing-xing/zhs`, therefore, a good regex is `huangdi-neijing/.+/zhs`. You can find the pattern from inspecting the HTML of the book's root page, in our example, it's https://ctext.org/huangdi-neijing/ling-shu-jing/zhs.
+
+I know this is not a user friendly way to filter. Sadly, I can't find any pattersn, like class name or page structure, that will work correctly.
+
+# NOTICE
+
+This source code is offered to public for free. Its purpose is education only. Anyone shouldn't use this software to download any significant content from ctext.org. Ctext stated that software downloading in large scale is prohibited and will result in banning. Please reference their orignial link (https://ctext.org/zhs) if you need to use Ctext's content in any way.
+
+Below is their statement from ctext.org website.
+
+```
+如果您想引用本网站上的内容，请同时加上至本站的链接：https://ctext.org/zhs。请注意：严禁使用自动下载软体下载本网站的大量网页，违者自动封锁，不另行通知。
+```
---
+++ crawl.py
@@ -0,0 +1,127 @@
+'''
+This crawler is for ctext.org, which is a website that hosts Chinese classics.
+'''
+
+from dataclasses import dataclass, asdict
+import requests
+import time
+import re
+import json
+import argparse
+from urllib.parse import urljoin
+from typing import Callable, List
+
+
+from bs4 import BeautifulSoup
+
+CTEXT_ROOT_URL = 'https://ctext.org'
+
+
+@dataclass
+class Chapter:
+    title: str
+    texts: List[str]
+
+
+@dataclass
+class Book:
+    name: str
+    chapters: List[Chapter]
+
+
+class BookCrawler:
+    @staticmethod
+    def fetch_html(url):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+        }
+        try:
+            print(f"Opening {url}")
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            time.sleep(1)
+            return response.text
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+
+    def __init__(self, url, title, chapter_href_filter: Callable[[str], bool], max_chapters=None):
+        self.root_url = url
+        self.title = title
+        self.chapter_href_filter = chapter_href_filter
+        self.max_chapters = max_chapters
+
+    def crawl_book(self) -> Book:
+        root_html = BookCrawler.fetch_html(self.root_url)
+        if root_html is None:
+            return "Failed to fetch root page."
+
+        soup = BeautifulSoup(root_html, 'html.parser')
+        chapters: List[Chapter] = []
+
+        chapter_idx = 1
+        for link in soup.find('div', id="content3").find_all('a'):
+            href = link.get('href')
+            # Modify this if needed for better filtering
+            if href and self.chapter_href_filter(href):
+                chapter_url = urljoin(CTEXT_ROOT_URL, href)
+                link_text = link.text.strip()
+                print(
+                    f"Crawling chapter {chapter_idx}: {link_text}, {chapter_url}")
+                chapter_idx += 1
+                chapter = self.crawl_chapter(chapter_url, title=link_text)
+                chapters.append(chapter)
+            else:
+                print(f"Skipping {link.text.strip()},  {href}")
+
+            if self.max_chapters and chapter_idx > self.max_chapters:
+                break
+        self.book = Book(name=self.title, chapters=chapters)
+        return self.book
+
+    def crawl_chapter(self, url, title="") -> Chapter:
+        chapter_html = BookCrawler.fetch_html(url)
+        assert chapter_html is not None, f"Failed to fetch chapter page from {url}"
+
+        s = BeautifulSoup(chapter_html, 'html.parser')
+        if not title:
+            title = s.find('h2').text.strip()
+        sections = [t.text.strip() for t in s.select("td[class='ctext']")]
+        # import pdb
+        # pdb.set_trace()
+
+        return Chapter(title=title, texts='\n'.join(sections))
+
+    def export_to_json(self, json_path=""):
+        if not json_path:
+            json_path = f"{self.title}.json"
+
+        d = [asdict(c) for c in self.book.chapters]
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(d, f)
+
+
+if __name__ == "__main__":
+    arg = argparse.ArgumentParser(description='''Crawl a book from ctext.org. Example:
+        python crawl.py --url="https://ctext.org/huangdi-neijing/ling-shu-jing/zhs" 
+          --title="黄帝内经 - 灵枢经" 
+          --chapter-filter-regex="huangdi-neijing/.+/zhs"
+    ''')
+    arg.add_argument('--url', required=True,
+                     help="The root url of the book, for example, https://ctext.org/huangdi-neijing/suwen/zhs")
+    arg.add_argument(
+        '--title', required=True, help="The title of the book, also the name of the output json file")
+    arg.add_argument('--chapter-filter-regex', type=str, required=True,
+                     help="A regex string to filter out chapter urls, for example, 'huangdi-neijing/.+/zhs'")
+    arg.add_argument('--max-chapters', type=int, default=None,
+                     help="The max number of chapters to crawl, useful for testing")
+    config = arg.parse_args()
+
+    crawler = BookCrawler(config.url,
+                          title=config.title,
+                          chapter_href_filter=lambda x: re.match(
+                              config.chapter_filter_regex, x) is not None,
+                          max_chapters=config.max_chapters)
+
+    crawler.crawl_book()
+    crawler.export_to_json()
